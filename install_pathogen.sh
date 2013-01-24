c1c18015a8ba9820324a28563a8fbaa6d240f5c7
Xiangyang Chu <xchu@redhat.com>
2013-01-24 15:02:51 +0800

    add simple script to install pathogen and bundles

---
+++ install_pathogen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+    curl -Sso ~/.vim/autoload/pathogen.vim \
+        https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+cd ~/.vim/bundle
+git clone git://github.com/scrooloose/nerdtree.git
+git clone git://github.com/majutsushi/tagbar.git
+git clone git://github.com/hynek/vim-python-pep8-indent.git
+
